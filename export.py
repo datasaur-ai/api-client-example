f498c5d57d99713f7a976827a92c074f38a01cfe
Dedy Prasetiady <dedy.berastagi@gmail.com>
2021-10-28 12:06:14 +0700

    Update export script use getExportDeliveryStatus

--- export.py
+++ export.py
@@ -1,66 +1,86 @@
+import json
+import os
+import time
 import requests
+import fire
+
+from urllib.parse import urlparse
 from oauthlib.oauth2 import BackendApplicationClient
 from requests_oauthlib import OAuth2Session
-from urllib.parse import urlparse
-import os
-import json
-import sys
-import re
-import time
 
-os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
-
-baseUrl = sys.argv[1]
-client_id = sys.argv[2]
-client_secret = sys.argv[3]
-projectId = sys.argv[4]
-exportFileName = sys.argv[5]
-exportFormat = sys.argv[6]
-outputDir = sys.argv[7]
-url = baseUrl + "/graphql"
-
-client = BackendApplicationClient(client_id=client_id)
-oauth = OAuth2Session(client=client)
-token = oauth.fetch_token(token_url= baseUrl + '/api/oauth/token', client_id=client_id,
-        client_secret=client_secret)
-
-with open('export.json', 'r') as file:
-    payloadString = file.read()
-    payload = json.loads(payloadString)
-  
-payload["variables"]["input"]["fileName"] = exportFileName
-payload["variables"]["input"]["projectIds"] = [ projectId ]
-payload["variables"]["input"]["format"] = exportFormat
-
-headers = {
-  'Authorization': 'Bearer ' + token['access_token'],
-  'Content-Type': 'application/json'
-}
+POOLING_INVERVAL_IN_MS = 500
 
 
-response = requests.request("POST", url, headers=headers, data = json.dumps(payload))
-if 'json' in response.headers['content-type']:
-  jsonResponse = json.loads(response.text.encode('utf8'))
-  print(json.dumps(jsonResponse, indent=1))
-  if len(jsonResponse["data"]["result"]["fileUrl"]) > 0: 
-    fileUrl = jsonResponse["data"]["result"]["fileUrl"]
-    os.makedirs(outputDir, exist_ok=True)
-  
+def export_project(base_url, client_id, client_secret, project_id, export_file_name, export_format, output_dir):
+    url = base_url + "/graphql"
+    access_token = get_access_token(base_url, client_id, client_secret)
+    operations = get_operations('export.json')
+
+    operations["variables"]["input"]["fileName"] = export_file_name
+    operations["variables"]["input"]["projectIds"] = [project_id]
+    operations["variables"]["input"]["format"] = export_format
+
+    response = post_request(url, access_token, operations)
+    if 'json' in response.headers['content-type']:
+        json_response = json.loads(response.text.encode('utf8'))
+        print(json.dumps(json_response, indent=1))
+
+        if len(json_response["data"]["result"]["fileUrl"]) > 0:
+            export_id = json_response["data"]["result"]["exportId"]
+            poll_export_delivery_status(url, access_token, export_id)
+
+            file_url = json_response["data"]["result"]["fileUrl"]
+            file_response = requests.request("GET", file_url)
+            os.makedirs(output_dir, exist_ok=True)
+            file_response_url = urlparse(file_url)
+            file_name = os.path.basename(file_response_url.path)
+            output_file = output_dir + '/' + file_name
+            open(output_file, 'wb').write(file_response.content)
+            print("Success downloading the file. Output file:" + output_file)
+    else:
+        print(response)
+
+
+def poll_export_delivery_status(url, access_token, export_id):
+    operations = get_operations('get_export_delivery_status.json')
+    operations["variables"]["exportId"] = export_id
     while True:
-      time.sleep(1)
-      fileResponse = requests.request("GET", fileUrl)
-      if fileResponse.status_code == 200:
-        fileResponseUrl = urlparse(fileUrl)
-        fileName = os.path.basename(fileResponseUrl.path)
-        outputFile = outputDir + '/' + fileName
-        open(outputFile, 'wb').write(fileResponse.content)
-        print("Success downloading the file. Output file:" + outputFile)
-        break;
-      elif fileResponse.status_code == 404:
-        print("Waiting for the file.")
-      elif fileResponse.status_code == 403 or  fileResponse.status_code >=500:
-        print("Stop waiting because error.")
-else:
-  print(response)   
-  
+        time.sleep(POOLING_INVERVAL_IN_MS)
+        response = post_request(url, access_token, operations)
+        if 'json' in response.headers['content-type']:
+            json_response = json.loads(response.text.encode('utf8'))
+            delivery_status = json_response["data"]["exportDeliveryStatus"]["deliveryStatus"]
+            if (delivery_status == "QUEUED"):
+                print("Waiting for exported file to be ready...")
+            elif (delivery_status == "DELIVERED"):
+                print("Exported file is ready")
+                break
+            elif (delivery_status == "FAILED"):
+                print("Failed to export file")
+                break
+        else:
+            print(response)
+            break
 
+
+def get_access_token(base_url, client_id, client_secret):
+    client = BackendApplicationClient(client_id=client_id)
+    oauth = OAuth2Session(client=client)
+    token = oauth.fetch_token(token_url=base_url + '/api/oauth/token', client_id=client_id,
+                              client_secret=client_secret)
+    return token['access_token']
+
+
+def get_operations(file_name):
+    with open(file_name, 'r') as file:
+        return json.loads(file.read())
+
+
+def post_request(url, access_token, operations):
+    headers = {'Authorization': 'Bearer ' + access_token, 'Content-Type': 'application/json'}
+    return requests.request("POST", url, headers=headers, data=json.dumps(operations))
+
+
+if __name__ == '__main__':
+    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+    fire.Fire(export_project)
