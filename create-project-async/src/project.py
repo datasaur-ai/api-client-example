9ccad42bd8de9ba394bd43d73d028857eac1da54
Ivan Marcellino <ivan.marcellino@gdplabs.id>
2021-12-16 11:23:14 +0700

    enumerate all document keys

--- create-project-async/src/project.py
+++ create-project-async/src/project.py
@@ -19,29 +19,31 @@ class Project():
         files = []
 
         # Set files in documents folder as input.documents
-        iterator = 0
-        for filepath in glob.iglob(f'{documents_path}/*'):
+        for iterator, filepath in enumerate(glob.iglob(f'{documents_path}/*')):
             file = (str(iterator), open(filepath, 'rb'))
             files.append(file)
             document = {
                 "fileName": filepath[10:],
                 "file": None
             }
-            payload_map[str(iterator)] = ["variables.input.documents." + str(iterator) + ".file"]
+            payload_map[str(iterator)] = [
+                "variables.input.documents." + str(iterator) + ".file"]
             documents.append(document)
             iterator += 1
 
-        # Use documents.settings and documents.docFileOptions from operations file
-        if (len(operations["variables"]["input"]["documents"]) > 0 and len(documents) > 0):
-            documents[0]["settings"] = operations["variables"]["input"]["documents"][0]["settings"]
-            documents[0]["docFileOptions"] = operations["variables"]["input"]["documents"][0]["docFileOptions"]
+        # apply other settings from operations file
+        for iterator, op_doc in enumerate(operations["variables"]["input"]["documents"]):
+            op_doc: dict
+            for key in op_doc.keys():
+                documents[iterator][key] = op_doc.get(key, None)
 
         operations["variables"]["input"]["documents"] = documents
 
         # Call Datasaur API
         headers = {'Authorization': 'Bearer ' + access_token}
         data = {'operations': json.dumps(operations), 'map': json.dumps(payload_map)}
-        response = requests.request("POST", url, headers=headers, data=data, files=files)
+        response = requests.request(
+            "POST", url, headers=headers, data=data, files=files)
         if 'json' in response.headers['content-type']:
             json_response = json.loads(response.text.encode('utf8'))
             if 'errors' in json_response:
