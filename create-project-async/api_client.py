88dc86ad45b269869ad262e4b9b9c39e15801b51
Ivan Marcellino <89899945+ivanm-gdp@users.noreply.github.com>
2022-11-04 22:44:21 +0700

    Support remote files in documents array (#6)
    * add Project.create_project_external_url
    
    - extract graphql request & response processing to separate function
    - add document list examples
    
    * fix: use single entry command for create-project
    
    * adjust readme

--- create-project-async/api_client.py
+++ create-project-async/api_client.py
@@ -4,24 +4,33 @@ from src.project import Project
 from src.job import Job
 
 
-def create_project(base_url, client_id, client_secret, team_id):
+def create_project(base_url, client_id, client_secret, team_id, documents_path="documents"):
     try:
         Project.create(
-            base_url, client_id, client_secret,
+            base_url,
+            client_id,
+            client_secret,
             team_id=str(team_id),
-            operations_path='project_configuration.json', documents_path='documents')
+            operations_path="project_configuration.json",
+            documents_path=documents_path
+        )
     except Exception as e:
         raise SystemExit(e)
 
 
 def get_job_status(base_url, client_id, client_secret, job_id):
     try:
-        Job.get_status(base_url, client_id, client_secret, job_id=str(
-            job_id), operations_path='src/get_job_status.json')
+        Job.get_status(
+            base_url,
+            client_id,
+            client_secret,
+            job_id=str(job_id),
+            operations_path="src/get_job_status.json",
+        )
     except Exception as e:
         raise SystemExit(e)
 
 
-if __name__ == '__main__':
-    environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+if __name__ == "__main__":
+    environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
     fire.Fire()
--- create-project-async/readme.md
+++ create-project-async/readme.md
@@ -9,12 +9,29 @@ python -m pip install -r src/requirements.txt
 
 ## Create Project
 
+### With local files, 
+Documents are located under `document/` folder
+
 ```
 python api_client.py create_project \
   --base_url BASE_URL \
   --client_id CLIENT_ID \
   --client_secret CLIENT_SECRET \
   --team_id TEAM_ID
+```
+
+### With List of URLs
+
+Provide a JSON file with `documents_path`
+Example file available under `documents-example.json`
+
+```
+python api_client.py create_project \
+  --base_url BASE_URL \
+  --client_id CLIENT_ID \
+  --client_secret CLIENT_SECRET \
+  --team_id TEAM_ID
+  --documents_path PATH_TO_DOCUMENTS_JSON
 ```
 
 ## Get Job Status
