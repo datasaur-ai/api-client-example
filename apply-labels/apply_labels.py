0ba6b60c9b5d3cbaa70a2185675c7b102601d4b4
Ivan Marcellino <ivan.marcellino@datasaur.ai>
2023-12-11 11:50:52 +0700

    adjust entrypoint functions

--- apply-labels/apply_labels.py
+++ apply-labels/apply_labels.py
@@ -1,12 +1,50 @@
 import fire
 from dotenv import load_dotenv
 
-from src.entrypoint import check_login_and_teams, validate_project
+from src.entrypoint import check_login_and_teams
+from src.helpers import GraphQLClient, load_jsonc, read_config
+from src.project import Project
 
 load_dotenv()
 
 
+def apply_labels(
+    team_id: str,
+    project_id: str,
+    labelers_file="labelers.json",
+    i_base_url: str | None = None,
+    i_client_id: str | None = None,
+    i_client_secret: str | None = None,
+    verbose: bool = False,
+):
+    """
+    The script will query the project's assignment and cabinet detail, then:
+    1. Replicate cabinet to each assigned member, if not exist
+    2. Open and apply cabinet for each assignee.
+
+    Uses BASSE_URL, CLIENT_ID, and CLIENT_SECRET from .env by default.
+    """
+    config = read_config(i_base_url, i_client_id, i_client_secret)
+    client = GraphQLClient(
+        base_url=config["base_url"],
+        client_id=config["client_id"],
+        client_secret=config["client_secret"],
+        verbose=verbose,
+    )
+
+    data = load_jsonc(labelers_file)
+
+    Project(client=client).apply_labels(
+        team_id=team_id,
+        project_id=project_id,
+        labelers=data["labelers"],
+    )
+
+
 if __name__ == "__main__":
     fire.Fire(
-        {"check_teams": check_login_and_teams, "validate_project": validate_project}
+        {
+            "check_teams": check_login_and_teams,
+            "apply_labels": apply_labels,
+        }
     )
--- apply-labels/src/entrypoint.py
+++ apply-labels/src/entrypoint.py
@@ -26,35 +26,3 @@ def check_login_and_teams(
     teams = Team(base_url=config["base_url"], client=client).fetch()
 
     pprint(teams)
-
-
-def validate_project(
-    team_id: str,
-    project_id: str,
-    labelers_file: str = "labelers.json",
-    i_base_url: str | None = None,
-    i_client_id: str | None = None,
-    i_client_secret: str | None = None,
-    verbose: bool = False,
-):
-    """
-    Get a project's assignment and cabinet detail
-    """
-    config = read_config(i_base_url, i_client_id, i_client_secret)
-    client = GraphQLClient(
-        base_url=config["base_url"],
-        client_id=config["client_id"],
-        client_secret=config["client_secret"],
-        verbose=verbose,
-    )
-
-    with open(labelers_file, "r") as f:
-        data = json5_load(f)
-        labelers = [l["email"] for l in data["labelers"]]
-
-    project = Project(client=client).validate(
-        team_id=team_id,
-        project_id=project_id,
-        labelers=labelers,
-    )
-    pprint(project)
