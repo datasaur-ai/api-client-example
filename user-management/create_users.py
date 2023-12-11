1cd5863e65ffdaac347f43bae0e6813f990b18f6
nangirvan <nangirvan98@gmail.com>
2023-12-11 14:31:55 +0700

    output the created users

--- user-management/create_users.py
+++ user-management/create_users.py
@@ -14,30 +14,35 @@ from toolbox.get_access_token import get_access_token
 from toolbox.post_request import post_request
 
 
-def create_users(base_url, client_id, client_secret, input_file_path="./sample-files/create_users_sample.csv", email_verified=0):
+def create_users(base_url, client_id, client_secret, input_file_path="./sample-files/create_users_input.csv", output_file_path="./sample-files/create_users_output.csv", email_verified=0):
     try:
         api_url = base_url + "/api/v1/users"
         access_token = get_access_token(base_url, client_id, client_secret)
 
         with open(input_file_path, newline='') as input_file:
-            users = csv.reader(input_file, delimiter=',')
-            for user in users:
-                new_user = {
-                    "email": user[0],
-                    "name": user[1],
-                    "password": user[2],
-                    "emailVerified": email_verified
-                }
-                response = post_request(api_url, access_token, new_user)
+            with open(output_file_path, 'w') as output_file:
+                users = csv.reader(input_file, delimiter=',')
+                writer = csv.writer(output_file)
 
-                if "json" in response.headers["content-type"]:
-                    json_response = response.json()
-                    if response.status_code == 200:
-                        pprint.pprint(json_response)
+                for user in users:
+                    new_user = {
+                        "email": user[0],
+                        "name": user[1],
+                        "password": user[2],
+                        "emailVerified": email_verified
+                    }
+                    response = post_request(api_url, access_token, new_user)
+
+                    if "json" in response.headers["content-type"]:
+                        json_response = response.json()
+                        if response.status_code == 200:
+                            user_id = json_response["data"]["id"]
+                            writer.writerow([user_id, new_user["email"], new_user["name"], new_user["password"]])
+                            pprint.pprint(json_response)
+                        else:
+                            raise Exception("{email} ERROR: {message}".format(email=new_user["email"], message=json_response["message"]))
                     else:
-                        raise Exception("{email} ERROR: {message}".format(email=new_user["email"], message=json_response["message"]))
-                else:
-                    raise Exception("{email} ERROR: {status_code}".format(email=new_user["email"], status_code=response.status_code))
+                        raise Exception("{email} ERROR: {status_code}".format(email=new_user["email"], status_code=response.status_code))
     except Exception as e:
         raise SystemExit(e)
 
--- user-management/readme.md
+++ user-management/readme.md
@@ -34,6 +34,7 @@ python create_users.py create_users \
     --client_id <CLIENT_ID> \
     --client_secret <CLIENT_SECRET> \
     --input_file_path <INPUT_FILE_PATH> \
+    --output_file_path <OUTPUT_FILE_PATH> \
     --email_verified <EMAIL_VERIFIED>
 ```
 
@@ -42,7 +43,8 @@ python create_users.py create_users \
 - `base_url` **(required)**: The base URL of the API endpoint.
 - `client_id` **(required)**: The client ID for authentication.
 - `client_secret` **(required)**: The client secret for authentication.
-- `input_file_path` **(required)**: Path to the CSV file containing user data. Default is "./sample-files/create_users_sample.csv".
+- `input_file_path` **(required)**: Path to the CSV file containing user data. Default is "./sample-files/create_users_input.csv".
+- `output_file_path` **(required)**: Path to the CSV file where the output data will be written. Default is "./output-files/create_users_output.csv".
 - `email_verified` **(optional)**: Flag indicating whether user emails are verified. Default is 0.
 
 #### Input File Format
@@ -53,10 +55,23 @@ The CSV file should contain the following data, in order, separated by commas:
 2. **User's Name:** The name of the user.
 3. **User's Password:** The password for the user.
 
-Example CSV file content (located at [`sample-files/create_users_sample.csv`](sample-files/create_users_sample.csv)):
+Example CSV file content (located at [`sample-files/create_users_input.csv`](sample-files/create_users_input.csv)):
 
 ```csv
 user1@email.com,UserName1,UserPa$$w0rd1
 user2@email.com,UserName2,UserPa$$w0rd2
+...
+```
+
+#### Output File Format
+
+The output file format is same with the input file format, but it have additional datas:
+1. **User's ID:** The ID of the created user.
+
+Example CSV file content (located at [`sample-files/create_users_output.csv`](sample-files/create_users_output.csv)):
+
+```csv
+1,user1@email.com,UserName1,UserPa$$w0rd1
+2,user2@email.com,UserName2,UserPa$$w0rd2
 ...
 ```
