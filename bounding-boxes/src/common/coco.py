01b267811e1a46f2530c71dbddbf936551e9a5b2
Ivan Marcellino <ivan.marcellino@datasaur.ai>
2024-04-23 22:19:09 +0700

    setup: dataclass representing COCO and DatasaurSchema format
    - minimal representation only

---
+++ bounding-boxes/src/common/coco.py
@@ -0,0 +1,57 @@
+from dataclasses import dataclass
+from typing import Dict, List
+
+
+@dataclass
+class License:
+    name: str
+    id: int
+    URL: str
+
+
+@dataclass
+class COCOCategory:
+    id: int
+    name: str
+    supercategory: str
+
+
+@dataclass
+class COCOImage:
+    id: int
+    width: float
+    height: float
+    file_name: str
+    license: int
+    flickr_url: str
+    coco_url: str
+
+
+@dataclass
+class COCOAnnotation:
+    id: int
+    image_id: int
+    category_id: int
+    """
+    Expected a list of 8 numbers, representing four vertices starting from top-left and going clockwise
+    """
+    segmentation: List[List[float]]
+
+    """
+    [x, y, width, height]
+    """
+    bbox: List[float]
+    area: int
+    """
+    int, but mainly 0/1
+    """
+    iscrowd: int
+    attributes: Dict[str, str | int | bool]
+
+
+@dataclass
+class COCO:
+    licenses: List[License]
+    categories: List[COCOCategory]
+    images: List[COCOImage]
+    annotations: List[COCOAnnotation]
---
+++ bounding-boxes/src/common/datasaur_schema.py
@@ -0,0 +1,90 @@
+from dataclasses import dataclass
+from typing import List, Optional, Dict
+
+
+@dataclass
+class GenericIdAndName:
+    id: Optional[str]
+    name: str
+
+
+@dataclass
+class Point:
+    x: float
+    y: float
+
+
+@dataclass
+class Shape:
+    pageIndex: int
+    points: List[Point]
+
+
+@dataclass
+class BBoxLabel:
+
+    id: str
+    bboxLabelClassId: str
+    bboxLabelClassName: str
+    caption: Optional[str]
+    shapes: List[Shape]
+    """
+    tba
+    """
+    answers: Optional[Dict[str, str | int | bool]]
+
+    # For Datasaur Schema used in input, we can ignore these
+    status: Optional[str]
+    labeledBy: Optional[str]
+    labeledByUserId: Optional[int]
+    acceptedByUserId: Optional[int]
+    rejectedByUserId: Optional[int]
+
+
+@dataclass
+class BBoxLabelClassQuestions:
+    """
+    tba
+    """
+
+    pass
+
+
+@dataclass
+class BBoxLabelClass(GenericIdAndName):
+    id: str
+    color: Optional[str]
+    captionAllowed: bool
+    captionRequired: bool
+    """
+    tba
+    """
+    questions: Optional[List[BBoxLabelClassQuestions]]
+
+
+@dataclass
+class BboxLabelSet(GenericIdAndName):
+    classes: List[BBoxLabelClass]
+
+
+@dataclass
+class Page:
+    pageIndex: int
+    pageHeight: int
+    pageWidth: int
+
+
+@dataclass
+class BBoxDatasaurSchemaData:
+    kinds: List[str]
+    pages: Optional[List[Page]]
+    bboxLabelSets: Optional[List[BboxLabelSet]]
+    bboxLabels: Optional[List[BBoxLabel]]
+    document: Optional[GenericIdAndName]
+    project: Optional[GenericIdAndName]
+
+
+@dataclass
+class DatasaurSchema:
+    data: BBoxDatasaurSchemaData
+    version: str
