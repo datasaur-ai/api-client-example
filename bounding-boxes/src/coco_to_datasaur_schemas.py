026d906a99f1c713a6209176cc142fd40792ed0d
Ivan Marcellino <ivan.marcellino@datasaur.ai>
2024-04-26 21:30:47 +0700

    reorder function: put coco_to_datasaur_schemas up top

--- bounding-boxes/src/coco_to_datasaur_schemas.py
+++ bounding-boxes/src/coco_to_datasaur_schemas.py
@@ -1,22 +1,104 @@
 import json
 from dataclasses import asdict
 from math import floor
-from typing import List
+from typing import Any, List
 
+from common.coco import COCO, COCOAnnotation
 from common.datasaur_schema import (
-    BBoxLabel,
     BBoxDatasaurSchemaData,
+    BBoxLabel,
     BBoxLabelClass,
     BboxLabelSet,
-    Shape,
-    Page,
-    Point,
     DatasaurSchema,
     GenericIdAndName,
+    Page,
+    Point,
+    Shape,
 )
-from common.coco import COCO, COCOAnnotation
 from common.scrub import scrub
 from dacite import from_dict
+
+
+def coco_to_datasaur_schemas(coco_json: Any) -> List[DatasaurSchema]:
+    """
+    One COCO JSON contains many images, hence the return value of this function needs to be a list of objects.
+
+    Since the return value is a list of Python dataclass object, we need to use dataclasses.asdict before json.dump-ing it.
+
+    See the main() function for an example usage.
+    """
+    # convert JSON dict to COCO representation
+    coco_object = from_dict(data=coco_json, data_class=COCO)
+
+    # validate segmentation first
+    for annot in coco_object.annotations:
+        for segmentation in annot.segmentation:
+            if len(segmentation) != 8:
+                raise Exception(
+                    f"expect segmentation to be a list-of-list of 8 elements, faulty annotation.id={annot.id}",
+                )
+
+    bbox_label_set = BboxLabelSet(
+        id=None,
+        name="BBox Label Set",
+        classes=bbox_label_classes_from_coco(coco_object),
+    )
+    images = coco_object.images
+
+    retval: List[DatasaurSchema] = []
+    for image in images:
+        annotations_by_images = [
+            annot for annot in coco_object.annotations if annot.image_id == image.id
+        ]
+
+        bbox_labels = [
+            bbox_label_from_coco_annotation(annot, bbox_label_set)
+            for annot in annotations_by_images
+        ]
+
+        schema = DatasaurSchema(
+            version="1",
+            data=BBoxDatasaurSchemaData(
+                kinds=["BBOX_BASED"],
+                bboxLabels=bbox_labels,
+                bboxLabelSets=[bbox_label_set],
+                document=GenericIdAndName(name=image.file_name, id=None),
+                project=None,
+                pages=[
+                    Page(
+                        pageIndex=0,
+                        pageHeight=floor(image.height),
+                        pageWidth=floor(image.width),
+                    )
+                ],
+            ),
+        )
+
+        retval.append(schema)
+
+    return retval
+
+
+def main() -> None:
+    sample_coco = "./samples/COCO.json"
+    with open(sample_coco) as f:
+        json_data = json.load(f)
+
+    schemas = coco_to_datasaur_schemas(json_data)
+
+    for index, schema in enumerate(schemas):
+        document = schema.data.document
+        filename = (
+            document.name.split(".")[0] + ".json"
+            if document is not None
+            else f"schema_{index}.json"
+        )
+
+        with open(filename, "w") as f:
+            schema_as_dict = asdict(schema)
+            json.dump(scrub(schema_as_dict), f)
+
+        break
 
 
 def bbox_label_classes_from_coco(
@@ -89,84 +171,6 @@ def bbox_label_from_coco_annotation(
         # TODO: support once custom attributes implemented
         answers=None,
     )
-
-
-def coco_to_datasaur_schemas(coco_object: COCO) -> List[DatasaurSchema]:
-    """
-    One COCO JSON contains many images, hence the return value of this function needs to be a list of objects
-    Since the return value is a list of Python dataclass object, we need to use dataclasses.asdict before json.dump-ing it. See the main() function for an example.
-    """
-    # validate segmentation first
-    for annot in coco_object.annotations:
-        for segmentation in annot.segmentation:
-            if len(segmentation) != 8:
-                raise Exception(
-                    f"expect segmentation to be a list-of-list of 8 elements, faulty annotation.id={annot.id}",
-                )
-
-    retval = []
-
-    bbox_label_set = BboxLabelSet(
-        id=None,
-        name="BBox Label Set",
-        classes=bbox_label_classes_from_coco(coco_object),
-    )
-
-    images = coco_object.images
-    for image in images:
-        annotations_by_images = [
-            annot for annot in coco_object.annotations if annot.image_id == image.id
-        ]
-
-        bbox_labels = [
-            bbox_label_from_coco_annotation(annot, bbox_label_set)
-            for annot in annotations_by_images
-        ]
-
-        schema = DatasaurSchema(
-            version="1",
-            data=BBoxDatasaurSchemaData(
-                kinds=["BBOX_BASED"],
-                bboxLabels=bbox_labels,
-                bboxLabelSets=[bbox_label_set],
-                document=GenericIdAndName(name=image.file_name, id=None),
-                project=None,
-                pages=[
-                    Page(
-                        pageIndex=0,
-                        pageHeight=floor(image.height),
-                        pageWidth=floor(image.width),
-                    )
-                ],
-            ),
-        )
-
-        retval.append(schema)
-
-    return retval
-
-
-def main() -> None:
-    sample_coco = "./samples/COCO.json"
-    with open(sample_coco) as f:
-        json_data = json.load(f)
-        coco_obj = from_dict(data=json_data, data_class=COCO)
-
-    schemas = coco_to_datasaur_schemas(coco_obj)
-
-    for index, schema in enumerate(schemas):
-        document = schema.data.document
-        filename = (
-            document.name.split(".")[0] + ".json"
-            if document is not None
-            else f"schema_{index}.json"
-        )
-
-        with open(filename, "w") as f:
-            schema_as_dict = asdict(schema)
-            json.dump(scrub(schema_as_dict), f)
-
-        break
 
 
 if __name__ == "__main__":
